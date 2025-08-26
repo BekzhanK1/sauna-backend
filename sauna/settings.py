7637980cc21ec451e9a413a02679119517eba2f1
BekzhanK1 <bkimadieff@gmail.com>
2025-08-26 13:30:31 +0500

    add api.vparu.kz

--- sauna/settings.py
+++ sauna/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["85.198.90.75", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["85.198.90.75", "api.vparu.kz", "localhost", "127.0.0.1"]
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
