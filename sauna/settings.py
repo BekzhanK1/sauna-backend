956d0244c23d654966ca6186e45223fd2e2252d0
BekzhanK1 <bkimadieff@gmail.com>
2025-09-05 13:49:59 +0500

    change to postgre

--- sauna/settings.py
+++ sauna/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = "sauna.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "sauna"),
+        "USER": os.getenv("POSTGRES_USER", "sauna"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "sauna"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
